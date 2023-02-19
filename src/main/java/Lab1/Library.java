11486ed6b9dedcc807029fd6cdf49e34b4a7f06e
WU Zhiang <WU Zhiang@10.89.171.182>
2023-02-19 13:30:53 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -4,8 +4,14 @@
 package Lab1;
 
 public class Library {
-	public static void main(String arg[]) {
+	/*public static void main(String arg[]) {
 		System.out.println("Hello, World!");
+	}*/
+
+	public static void main(String arg[]) {
+		Library lib = new Library();
+		if (lib.someLibraryMethod()) 
+			System.out.println("Hello, Worlddd!");
 	}
 	
     public boolean someLibraryMethod() {
